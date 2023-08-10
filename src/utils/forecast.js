8d7032d2b055e08efb0b81485e1327813b93e2a9
Hammad Mardani <h.mardani@reply.com>
2023-08-10 10:58:38 +0100

    fixing forecast and adding humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,17 +3,14 @@ const request = require("request")
 const forecast=(long,lat,callback)=>{
     const url = "http://api.weatherstack.com/current?access_key=6eeba8e9396e9b672803e1d26283cd20&query="+encodeURIComponent(lat)+","+encodeURIComponent(long)+"&units=m"
 
-    request({url:url, json:true},(error,{body})=>{
+    request({url, json:true},(error,{body})=>{
         if (error){
             callback("Unable to connect to weather services",undefined)
         } else if(body.error){
             callback("Unable to find location, try another search",url)
         }else{
-            callback(undefined,{
-                description : body.current.weather_descriptions[0],
-                temp: body.current.temperature,
-                feelslike: body.current.feelslike
-            })
+            console.log(body.current)
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently: " +body.current.temperature + " degrees out. It feels like " +body.current.feelslike+ " degrees. The humidity today is: "+body.current.humidity)
         }
     })
 
